fad6b6ad347eec596f0fe5473cc79eeef3624b3a
ishrak-liberatelabs <ishrak@liberate-labs.com>
2023-07-19 22:42:49 +0600

    resolve

--- app/service/chat.ts
+++ app/service/chat.ts
@@ -1,5 +1,10 @@
-const url= process.env.FLOWISE_URL
-export async function query(data) {
+const url: string = process.env.FLOWISE_URL || ""; // Provide a default value for the URL if it is not available
+
+interface QueryData {
+  question: string;
+}
+
+export async function query(data: QueryData): Promise<any> {
   const response = await fetch(
     url,
     {
@@ -7,10 +12,10 @@ export async function query(data) {
       headers: {
         "Content-Type": "application/json",
       },
-
       body: JSON.stringify(data),
     }
   );
+
   const result = await response.json();
 
   return result;
