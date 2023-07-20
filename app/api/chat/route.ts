cc0c4ad7ccb1b8421d966403a49abc05e4b446e8
ishrak-liberatelabs <ishrak@liberate-labs.com>
2023-07-20 10:54:57 +0600

    commenting

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,27 +1,27 @@
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  const url: string = process.env.FLOWISE_URL || "";
-  const request = await req.json();
+  const url: string = process.env.FLOWISE_URL || ""; // Get the FLOWISE_URL from the environment variables, or use an empty string as a fallback
+  const request = await req.json(); // Parse the incoming request body as JSON
 
-  console.log(request);
+  console.log(request); // Log the parsed request body to the console
 
-  let result: any;
+  let result: any; // Initialize a variable to store the response from the FLOWISE_URL
 
   try {
-    const response = await fetch(url, {
+    const response = await fetch(url, { // Send a POST request to the FLOWISE_URL with the parsed request as the body
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json", // Set the request header to indicate that the body contains JSON data
       },
-      body: JSON.stringify(request),
+      body: JSON.stringify(request), // Convert the parsed request body to JSON string
     });
 
-    result = await response.json();
-    console.log(result);
-  } catch (error: any) {
-    result = error?.code || "";
+    result = await response.json(); // Parse the response from the FLOWISE_URL as JSON
+    console.log(result); // Log the parsed response to the console
+  } catch (error: any) { // Catch any errors that may occur during the fetch operation
+    result = error?.code || ""; // If an error occurs, store the error code (if available) or an empty string
   }
 
-  return NextResponse.json(result);
+  return NextResponse.json(result); // Return a JSON response using the NextResponse object containing the result data
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 import { query } from "./service/chat";
 
 export default function Chat() {
-  const [messages, setMessages] = useState<string[]>([]); // Type annotation added for "messages"
-  const [message, setMessage] = useState("");
-  const [client, setClient] = useState<string[]>([]);
+ const [messages, setMessages] = useState<string[]>([]); // Stores the chat messages
+  const [message, setMessage] = useState(""); // Stores the current user input message
+  const [client, setClient] = useState<string[]>([]); // Stores the source of each message (User or AI)
 
+  // Function to send a message to the backend
   const getMessage = async (input: any) => {
     console.log(input);
     const res = await fetch("api/chat", {
@@ -16,60 +17,69 @@ export default function Chat() {
     return res;
   };
 
+  // Function to handle the form submission (when the user sends a message)
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
+    // Update the state to store the user's message
     setMessages((current) => [...current, message]);
     setClient((current) => [...current, "user : "]);
 
+    // Send the user's message to the backend and wait for the response
     getMessage({ question: message }).then(async (response) => {
-      const data = await response.json();
-      console.log(data);
+      try {
+        const data = await response.json();
+        console.log(data);
 
-      if (data === "") {
+        if (data === "") {
+          return;
+        }
+
+        // Update the state to store the AI's response
+        setMessages((current) => [...current, data]);
+        setClient((current) => [...current, "AI : "]);
+      } catch (error) {
+        // If an error occurs, display a fallback message
         setMessages((current) => [...current, "I am unavailable right now!"]);
         setClient((current) => [...current, "AI : "]);
-        return;
       }
-      setMessages((current) => [...current, data]);
-      setClient((current) => [...current, "AI : "]);
     });
 
-    // query({ question: message }).then((response) => {
-
-    // });
-
+    // Clear the input field after sending the message
     setMessage("");
   };
 
+
   return (
     <div className="mx-auto w-full max-w-md py-24 flex flex-col h-screen">
-      {messages.length > 0 ? (
-        messages.map((m, index) => (
-          <div
-            className="flex p-3 shadow-lg mb-4 rounded-lg bg-slate-50"
-            key={(index + 1) * 2000}
-          >
-            <div className="mx-3 w-14" key={(index + 1) * 10}>
-              {client[index]}
-            </div>
-            <div key={index} className="w-80">
-              {" "}
-              {m}
-            </div>
+  {messages.length > 0 ? ( // Check if there are any messages to display
+    messages.map((m, index) => // Map through the messages array and display each message
+      (
+        <div
+          className="flex p-3 shadow-lg mb-4 rounded-lg bg-slate-50" // CSS classes for styling the chat message container
+          key={(index + 1) * 2000} // React key to uniquely identify each chat message
+        >
+          <div className="mx-3 w-14" key={(index + 1) * 10}> // Styling for the client/source of the message (User or AI)
+            {client[index]} // Display the client/source of the message (User or AI) based on the index
           </div>
-        ))
-      ) : (
-        <div>No messages yet.</div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed w-full max-w-md bottom-10 p-2 rounded-lg border border-gray-100 shadow-lg outline-none"
-          placeholder="Say Something..."
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-        />
-      </form>
-    </div>
+          <div key={index} className="w-80"> // Styling for the actual message content
+            {" "}
+            {m} // Display the message content
+          </div>
+        </div>
+      )
+    )
+  ) : (
+    <div>No messages yet.</div> // If there are no messages, display a message indicating that there are no messages yet
+  )}
+  <form onSubmit={handleSubmit}> // Form to submit new user messages
+    <input
+      className="fixed w-full max-w-md bottom-10 p-2 rounded-lg border border-gray-100 shadow-lg outline-none"
+      placeholder="Say Something..." // Placeholder text for the input field
+      value={message} // Bind the input value to the "message" state variable
+      onChange={(event) => setMessage(event.target.value)} // Handle changes in the input field and update the "message" state variable
+    />
+  </form>
+</div>
   );
 }
